b2b34551e9468a74a1e425bf13491f183cff48d1
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-04-27 01:58:21 -0500

    Power point and demo code for CI

---
+++ DVCSandCI/ContinuousIntegrationSample/ContinuousIntegrationSample/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace ContinuousIntegrationSample
+{
+	using NUnit.Framework;
+
+	public class Calculator
+	{
+		public decimal Add(int a, int b)
+		{
+			return a + b;
+		}
+	}
+
+	[TestFixture]
+	public class CalculatorTests : AssertionHelper
+	{
+		[Test]
+		public void Add_TwoPositiveIntegers_ReturnsExpectedResult()
+		{
+			var a = 5;
+			var b = 4;
+			var expectedResult = 9;
+			var calculator = new Calculator();
+
+			var actualResult = calculator.Add(a, b);
+
+			Expect(actualResult, Is.EqualTo(expectedResult));
+		}
+	}
+}
