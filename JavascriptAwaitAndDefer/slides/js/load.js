ed4c397cddc28df48476c417eb1282803c4b93e2
Wesley McClure <wes.mcclure@gmail.com>
2013-04-16 13:12:39 -0500

    slides work

--- JavascriptAwaitAndDefer/slides/js/load.js
+++ JavascriptAwaitAndDefer/slides/js/load.js
@@ -1,17 +1,26 @@
-Reveal.initialize({
-    controls: true,
-    progress: true,
-    history: false,
-    keyboard: true,
-    overview: true, // use esc key
-    center: true,
-    loop: false,
-    rtl: false,
-    autoSlide: 0,
-    mouseWheel: false,
-    rollingLinks: true,
-    transition: 'cube', // default/cube/page/concave/zoom/linear/fade/none
-    transitionSpeed: 'default' // default/fast/slow
-});
+$(function () {
+    $('section.bad').attr('data-state', 'bad');
+    $('section.good').attr('data-state', 'good');
+    $('section.caution').attr('data-state', 'caution');
 
-prettyPrint();
+    Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        keyboard: true,
+        overview: true, // use esc key
+        center: true,
+        loop: false,
+        rtl: false,
+        autoSlide: 0,
+        mouseWheel: false,
+        rollingLinks: true,
+        transition: 'cube', // default/cube/page/concave/zoom/linear/fade/none
+        transitionSpeed: 'default' // default/fast/slow
+    });
+
+    $('a').attr('target', '_blank');
+
+    $('pre').addClass('prettyprint');
+    prettyPrint();
+})
