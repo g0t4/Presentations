7810a33f8814b76e68d0d463e630b955c4ca104a
Wesley McClure <wes.mcclure@gmail.com>
2013-04-16 10:13:46 -0500

    some slides to organize content that is outside code

---
+++ JavascriptAwaitAndDefer/slides/js/load.js
@@ -0,0 +1,15 @@
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: false,
+    keyboard: true,
+    overview: true, // use esc key
+    center: true,
+    loop: false,
+    rtl: false,
+    autoSlide: 0,
+    mouseWheel: false,
+    rollingLinks: true,
+    transition: 'cube', // default/cube/page/concave/zoom/linear/fade/none
+    transitionSpeed: 'default', // default/fast/slow
+});
