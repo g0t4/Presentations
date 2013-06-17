eba92f86b955a306180749df3b6726d257ca514c
Wesley McClure <wes.mcclure@gmail.com>
2013-06-17 13:11:35 -0500

    add some more tests so I can refactor more :)

--- NodeCodeReview/craigslist/test/htmlScraperTests.js
+++ NodeCodeReview/craigslist/test/htmlScraperTests.js
@@ -16,5 +16,18 @@ describe('html scraper', function() {
     it('should have the price', function() {
       expect(listings[0].price).to.equal('$50000');
     });
+
+    it('should have the postId', function() {
+      expect(listings[0].postId).to.equal('1234');
+    });
+
+    it('should have the title', function() {
+      expect(listings[0].title).to.equal('OMG THE BEST APARTMENT EVAR NO LIES!!!');
+    });
+
+    it('should have the url', function() {
+      expect(listings[0].url).to.equal('/see/apa/1234.html');
+    });
+
   })
 })
